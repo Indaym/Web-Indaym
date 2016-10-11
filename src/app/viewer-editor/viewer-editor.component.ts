5401e918e2077bd8e4c69be84085543135d8c53b
Nicolas Delahaigue <nicolas.delahaigue@epitech.eu>
2016-10-11 10:33:12 +0300

    update viewer

--- src/app/viewer-editor/viewer-editor.component.ts
+++ src/app/viewer-editor/viewer-editor.component.ts
@@ -3,7 +3,7 @@
  */
 
 import { Component, OnInit }    from '@angular/core';
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, Vector3 } from 'three';
 
 @Component({
     selector  : 'editor',
@@ -18,11 +18,14 @@ export class ViewerEditorComponent implements OnInit {
         var scene = new Scene();
         var camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
         var geometry = new BoxGeometry( 1, 1, 1 );
-        var material = new MeshBasicMaterial( { color: 0x00ff00 } );
+        var material = new MeshBasicMaterial( { color: 0x999999} );
         var cube = new Mesh( geometry, material );
         scene.add( cube );
 
         camera.position.z = 5;
+        camera.position.x = 5;
+        camera.position.y = 5;
+        camera.lookAt(new Vector3(0, 0, 0));
         var renderer = new WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.getElementById('editorContainer').appendChild( renderer.domElement );
