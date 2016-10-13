a4be5a760f65da9d5dfd5618306c04e217ab28ee
Tokiro <tokiro@hotmail.fr>
2016-10-13 16:19:42 +0800

    [Non-définitif] Maj du dernier commit de test

--- src/app/components/forum-component.ts
+++ src/app/components/forum-component.ts
@@ -1,8 +1,9 @@
 import { Component }    from '@angular/core';
+import { SearchComponent }     from '../views/forum/search/search.component';
 
 @Component({
   selector  : 'forum-component',
-  template  : require('app/views/forum.html'),
+  template  : require('app/views/forum/forum.html'),
   styleUrls    : ['app/styles/forum.css'],
   providers : []
 })
