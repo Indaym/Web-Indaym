2100daea6b6ee547da95e4a39567b7a4f8c0ed0b
djavrell <kevin.prouteau@epitech.eu>
2016-10-26 21:43:12 +0800

    update karma.conf

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -2,31 +2,49 @@
  * @author: @ProuteauK
  */
 
-module.exports = function(config) {
-  var testWebpackConfig = require('./webpack.test.js');
+module.exports = function (config) {
+  var testWebpackConfig = require ('./webpack.test.js');
 
-  config.set({
+  config.set ({
     basePath: '',
-    frameworks: ['jasmine'],
-    exclude: [ ],
-    files: [ { pattern: './config/spec-bundle.js', watched: false } ],
-    preprocessors: { './config/spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
+    frameworks: [
+      'jasmine'
+    ],
+    exclude: [],
+    files: [{
+      pattern: './config/spec-bundle.js',
+      watched: false
+    }],
+    preprocessors: {
+      './config/spec-bundle.js': [
+        'coverage',
+        'webpack',
+        'sourcemap'
+      ]
+    },
     webpack: testWebpackConfig,
     coverageReporter: {
-      dir : 'coverage/',
+      dir: 'coverage/',
       reporters: [
         { type: 'text-summary' },
         { type: 'json' },
         { type: 'html' }
       ]
     },
-    webpackServer: { noInfo: true },
-    reporters: [ 'mocha', 'coverage' ],
+    webpackServer: {
+      noInfo: true
+    },
+    reporters: [
+      'mocha',
+      'coverage'
+    ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: [ 'PhantomJS' ],
+    browsers: [
+      'PhantomJS'
+    ],
     singleRun: true
   });
 };
