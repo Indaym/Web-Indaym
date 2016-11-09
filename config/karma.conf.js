1b4fcfa953861ff560c223279447e9e1d78bb675
djavrell <kevin.prouteau@epitech.eu>
2016-11-09 10:51:15 +0800

    setup karma testing
    es6 feature don't work with phantomJS yet

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,46 +1,55 @@
+const helper = require('./helpers');
+
 module.exports = function (config) {
   var testWebpackConfig = require ('./webpack.test.js');
 
   config.set ({
-    basePath: '',
+    basePath: helper.root('src'),
     frameworks: [
-      'jasmine'
+      'jasmine',
+      'es6-shim'
+    ],
+    plugin: [
+      'karma-phantomjs-launcher',
+      'karma-es6-shim',
+      'karma-babel-preprocessor',
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'coverage',
+      'webpack',
+      'sourcemap'
     ],
     exclude: [],
-    files: [{
-      pattern: './config/spec-bundle.js',
-      watched: false
-    }],
+    files: [
+      { pattern: './config/spec-bundle.js', watched: false },
+      { pattern: '**/*.spec.ts'}
+    ],
     preprocessors: {
-      './config/spec-bundle.js': [
-        'coverage',
-        'webpack',
-        'sourcemap'
-      ]
+      './config/spec-bundle.js': [ 'coverage', 'webpack', 'sourcemap' ],
+      'src/**/*.ts': [ 'typescript' ]
     },
+    typescriptPreprocessor: {
+      option: { target: 'ES5' },
+      transformPath: (path) => path.replace(/\.ts$/, '.js')
+    },
+    reporters: [ 'progress', 'dots', 'coverage' ],
+    hostname: 'localhost',
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: [ 'Chrome' ],
+    singleRun: true,
     webpack: testWebpackConfig,
     coverageReporter: {
-      dir: 'coverage/',
+      dir: '../coverage/',
       reporters: [
         { type: 'text-summary' },
         { type: 'json' },
         { type: 'html' }
       ]
     },
-    webpackServer: {
-      noInfo: true
-    },
-    reporters: [
-      'mocha',
-      'coverage'
-    ],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: [
-      'PhantomJS'
-    ],
-    singleRun: true
+    webpackServer: { noInfo: true },
   });
 };
---
+++ src/app/1st.e2e.ts
@@ -0,0 +1,5 @@
+describe('1st test', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+});
