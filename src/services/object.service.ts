d8d0d2c745da56eb27497ad5cfb7c40abe4d6d10
CAVAL93 <caroline.vallerie.1@ulaval.ca>
2017-01-08 19:00:21 -0500

    Removed debug Renamed baka to dataObjects

--- src/services/object.service.ts
+++ src/services/object.service.ts
@@ -2,13 +2,13 @@
  * Created by Caro on 08/01/2017.
  */
 import { Injectable } from '@angular/core';
-import { DATAS } from './baka.ts';
+import { DATAS } from './dataObjects.ts';
 
 @Injectable()
 export class ObjectService {
     getObjects() {
         //faire l'appel à l'API ici, lol
-        console.log("pouet");
+        console.log("loading objects");
         return DATAS;
     }
 }
