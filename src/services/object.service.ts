c3fa025d1d2d431e95e79a0e6e844f13c8be4561
CAVAL93 <caroline.vallerie.1@ulaval.ca>
2017-02-01 23:21:03 -0500

    Synchro items with API

--- src/services/object.service.ts
+++ src/services/object.service.ts
@@ -2,22 +2,22 @@
  * Created by Caro on 08/01/2017.
  */
 import { Injectable } from '@angular/core';
-import { DATAS } from './dataObjects.ts';
-import { Headers, Http } from '@angular/http';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ObjectService {
     private objectsUrl = 'http://localhost:3000/library';
-    //private objectsUrl = 'localhost:3000/library';
+    private objects = [];
 
     constructor(private http: Http) { }
 
     getObjects() {
         console.log("loading objects");
-        this.http.get(this.objectsUrl).toPromise().then((response) => { console.log(response.json()); });
-        //console.log(this.http.get(this.objectsUrl).toPromise().then((response => response.json())));
-        //return this.http.get(this.objectsUrl);
-        return DATAS;
+
+        this.http.get(this.objectsUrl)
+            .flatMap((res) => res.json())
+            .subscribe(data => {this.objects.push(data);});
+        return this.objects;
     }
 }
