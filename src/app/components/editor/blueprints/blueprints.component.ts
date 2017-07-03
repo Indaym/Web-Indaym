1ab2e4fccd982b3912fb11d0aa4aa3d9ea80df12
Caroline Vallerie <caroline.vallerie.1@ulaval.ca>
2017-07-03 16:11:18 +0200

    Synchro fail wrong uuid

--- src/app/components/editor/blueprints/blueprints.component.ts
+++ src/app/components/editor/blueprints/blueprints.component.ts
@@ -1,18 +1,50 @@
-import { Component }        from '@angular/core';
-import { EventDispatcher }  from 'three';
+import { Component }              from '@angular/core';
+import { EventDispatcher }        from 'three';
+import { GameControllerService }  from '../../../../services';
+import { ObjectService } from "../../../../services/object.service";
 
 @Component({
   selector  : 'ia-blueprints',
   template  : require('./blueprints.component.html'),
   styles    : [
-    require('./blueprints.component.css'),
+    require('./blueprints.component.css')
   ],
-  providers : [],
+  providers : []
 })
 export class BlueprintsComponent {
   private dispatcher: EventDispatcher;
+  public  selectRules = (id) => {this.affRules(id)};
+  private gameController;
+  private objs;
+  private objInfo;
+  private rules;
 
-  constructor() {
+  constructor(private gameControllerService:GameControllerService, private objectService:ObjectService) {
     this.dispatcher = new EventDispatcher();
+    this.gameController = this.gameControllerService.gameController;
+    this.objs = this.gameController.getObjects();
+  }
+
+  private affRules(id) {
+    this.objInfo = this.objs.find((lm, index, array) => {
+      if (lm.uuid == id)
+        return true;
+      else
+        return false;
+    });
+    console.log("affRules");
+    console.log(this.objInfo);
+    console.log(id);
+    this.saveRules();
+  }
+
+  private saveRules() {
+    this.objInfo.object["rules"] = [];
+
+    this.rules = document.getElementById("rulesContainer");
+    console.log(this.rules);
+
+    this.objectService.updateObject({object:this.objInfo.object}, this.objInfo.uuid);
+    console.log("update done");
   }
 }
--- src/app/components/editor/blueprints/sidebar/left-bar/left-bar.component.ts
+++ src/app/components/editor/blueprints/sidebar/left-bar/left-bar.component.ts
@@ -3,15 +3,14 @@
  */
 
 import {
-  Component,
-  Input,
-  OnInit,
+    Component,
+    Input, OnInit
 } from '@angular/core';
 
 import {
-  HtmlService,
-  GameControllerService,
-} from '../../../../../../services';
+    HtmlService,
+    GameControllerService,
+} from "../../../../../../services";
 
 @Component({
   selector  : 'ia-left-bar',
@@ -19,22 +18,31 @@ import {
   template  : require('./left-bar.component.html'),
   styles    : [
     require('./left-bar.component.css'),
-    require('../../../designer/sidebar/sidebars.css'),
-  ],
+    require('../../../designer/sidebar/sidebars.css')
+  ]
 })
-export class LeftBarComponent implements OnInit {
-  @Input() public menu;
-  @Input() public eventDispatcher;
+export class LeftBarComponent implements OnInit{
+  @Input() menu;
+  @Input() eventDispatcher;
+  @Input() selectRules;
+  @Input() objs;
 
   private gameController;
 
-  constructor(public html: HtmlService, private gameControllerService: GameControllerService) {
+  constructor(public html: HtmlService, private gameControllerService:GameControllerService) {
     this.gameController = this.gameControllerService.gameController;
+    console.log("leftbarcomponent");
+    console.log(this.objs);
   }
 
-  public ngOnInit() {}
+  ngOnInit() {}
 
   private toggleMode() {
-    this.menu.mode = (this.menu.mode === 'side') ? 'over' : 'side';
+    this.menu.mode = (this.menu.mode == 'side') ? 'over' : 'side';
+  }
+
+  private loadRules(objId) {
+    console.log("hello nico" + objId);
+    this.selectRules(objId);
   }
 }
