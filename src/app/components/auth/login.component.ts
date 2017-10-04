1548bf92acbd83d98525740be7679a0331004022
djavrell <kevin.prouteau@epitech.eu>
2017-10-04 15:43:53 +0200

    extract template from login and register component ts files into their own template files

--- src/app/components/auth/login.component.ts
+++ src/app/components/auth/login.component.ts
@@ -10,34 +10,7 @@ import { AuthService } from '../../services';
 @Component({
   selector: 'ia-login',
   providers: [ AuthService ],
-  template: `
-  <div *ngIf="!hasError">
-    <form #loginForm="ngForm" (ngSubmit)="login()" >
-      <div>
-        <label for="name" >Username</label>
-        <input type="text" id="name" required [(ngModel)]="username" name="name" #name="ngModel" >
-      </div>
-      <div [hidden]="name.valid || name.pristine" >Username is required</div>
-
-      <div>
-        <label for="Email" >email</label>
-        <input type="email" id="Email" required [(ngModel)]="email" name="Email" #Email="ngModel" >
-      </div>
-      <div [hidden]="Email.valid || Email.pristine" >Email is required</div>
-
-      <div>
-        <label for="Passwod" >Password</label>
-        <input type="password" id="Passwod" required [(ngModel)]="password" name="Passwod" #Passwod="ngModel" >
-      </div>
-      <div [hidden]="Passwod.valid || Passwod.pristine" >Password is required</div>
-
-      <button type="submit" [disabled]="!loginForm.form.valid">Submit</button>
-    </form>
-  </div>
-  <div *ngIf="hasError" >
-    {{error}}
-  </div>
-  `,
+  templateUrl: './login.component.html',
   styleUrls: [],
 })
 export class LoginComponent implements OnInit {
