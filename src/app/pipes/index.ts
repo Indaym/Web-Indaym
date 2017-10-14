903eec144c16621b58efb7005ca176bc566f70ca
Nicolas Delahaigue <nicolas.delahaigue@epitech.eu>
2017-10-15 00:20:51 +0200

    Add hex pipe

--- src/app/pipes/index.ts
+++ src/app/pipes/index.ts
@@ -1,5 +1,7 @@
-import { KeysPipe } from './keys.pipe';
+import { KeysPipe }       from './keys.pipe';
+import { ToHexViewPipe }  from './to-hex-view.pipe';
 
 export const PIPES = [
   KeysPipe,
+  ToHexViewPipe,
 ];
---
+++ src/app/pipes/to-hex-view.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'toHexView',
+})
+export class ToHexViewPipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    return value.replace('0x', '#');
+  }
+}
