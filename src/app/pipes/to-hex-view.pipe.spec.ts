accd20d15ac88c5600a9853f19f065d201895d96
Nicolas Delahaigue <nicolas.delahaigue@epitech.eu>
2017-11-06 20:28:18 +0100

    Delete AGAIN these files

--- src/app/pipes/to-hex-view.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToHexViewPipe } from './to-hex-view.pipe';
-
-describe('ToHexViewPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToHexViewPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
