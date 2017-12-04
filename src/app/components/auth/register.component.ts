93dca8f6c7cc50f34910e59467aec531e36c6aee
Prouteau Kévin <kevin.prouteau@epitech.eu>
2017-12-04 23:02:37 +0100

    begin register form

--- src/app/components/auth/register.component.ts
+++ src/app/components/auth/register.component.ts
@@ -6,19 +6,33 @@ import { Router }  from '@angular/router';
 
 import { AuthService } from '../../services';
 
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  FormBuilder,
+  ValidatorFn,
+  AbstractControl,
+}                       from '@angular/forms';
+import { ErrorMatcher } from './ErrorMatcher';
+
 @Component({
   selector: 'ia-register',
   providers: [ AuthService ],
   templateUrl: './register.component.html',
   styleUrls: [
-    './login.component.css',
+    // './login.component.css',
   ],
 })
 export class RegisterComponent implements OnInit {
   constructor(
     private auth: AuthService,
     private router: Router,
+    private fb: FormBuilder,
   ) {}
+
+  registerForm: FormGroup;
+  matcher: ErrorMatcher = new ErrorMatcher();
 
   success = false;
   hasError = false;
@@ -40,11 +54,27 @@ export class RegisterComponent implements OnInit {
     this.hasError = true;
   }
 
+  checkPassword(control: AbstractControl): { [key: string]: any } {
+    const p1 = control.get('password').value;
+    const p2 = control.get('confirm_password').value;
+    if (p1 === p2) return null;
+    return { nomatch: true };
+  }
+
   ngOnInit() {
     this.auth.reset();
+    this.registerForm = this.fb.group({
+      'username': [ this.username, Validators.required ],
+      'email': [ this.email, [ Validators.required, Validators.email ] ],
+      'passwords': this.fb.group({
+        'password': [ this.password, Validators.required ],
+        'confirm_password': [ this.confirmation_password, Validators.required ],
+      }, { validator: this.checkPassword }),
+    });
   }
 
   register() {
+    console.log(this.registerForm);
     this.hasError = false;
     if (this.password !== this.confirmation_password) {
       this.pwdIsOk = false;
