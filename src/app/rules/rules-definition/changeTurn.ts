dcecd7290a13f9e06f7dde0c4fd73c40977c9d9c
Tokiro <tokiro@hotmail.fr>
2018-01-08 14:14:38 +0100

    Change turn with team number and add team number on object

--- src/app/rules/rules-definition/changeTurn.ts
+++ src/app/rules/rules-definition/changeTurn.ts
@@ -22,8 +22,8 @@ export class ChangeTurn extends BaseRules {
   public run(args?: any): boolean {
     if (this._refObj.threeDModel.oldPosition[0] === -1 && this._refObj.threeDModel.oldPosition[1] === -1)
       return true;
-    if (([0, 2].includes(this._refScene.player) && this._refObj.name !== 'whitepawn') ||
-    ([1].includes(this._refScene.player) && this._refObj.name !== 'blackpawn'))
+    if (([0, 2].includes(this._refScene.player) && this._refObj.object.team !== 0) ||
+    ([1].includes(this._refScene.player) && this._refObj.object.team !== 1))
       return false;
     return true;
   }
