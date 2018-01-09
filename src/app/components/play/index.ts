b9d56e970cf1ef52e7b845b7a3e1123db3f328f7
Nicolas Delahaigue <nicolas.delahaigue@epitech.eu>
2018-01-09 22:09:57 +0100

    You saw nothing

--- src/app/components/play/index.ts
+++ src/app/components/play/index.ts
@@ -2,5 +2,5 @@
  * Created by nicolas on 19/10/16.
  */
 
-export { PlayComponent, comeFrom } from './play.component';
-export { PlayerComponent } from './player.component';
+export { PlayComponent, comeFrom }  from './play.component';
+export { PlayerComponent }          from './player.component';
