f194d4adb840e5651d0b39957574bcaf3d33382f
Prouteau Kévin <kevin.prouteau@epitech.eu>
2018-01-20 10:36:17 +0100

    pre build prod

--- config/server.conf.ts
+++ config/server.conf.ts
@@ -1,6 +1,8 @@
 export const serverConfig = {
 //  serverURL: 'http://localhost:3000/',
 //  server: 'http://localhost:3000',
-  serverURL: "http://app-8e4e5256-4f84-4386-9a09-008c5a7e06db.cleverapps.io/",
-  server: "http://app-8e4e5256-4f84-4386-9a09-008c5a7e06db.cleverapps.io"
+  //serverURL: "http://app-8e4e5256-4f84-4386-9a09-008c5a7e06db.cleverapps.io/",
+  //server: "http://app-8e4e5256-4f84-4386-9a09-008c5a7e06db.cleverapps.io"
+  serverURL: "http://indaymapi.cleverapps.io/",
+  server: "http://indaymapi.cleverapps.io",
 };
